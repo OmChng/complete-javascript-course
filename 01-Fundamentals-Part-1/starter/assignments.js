dd760eaa32a52e97daad54331c741f08fe543904
OChng <44788682+OmChng@users.noreply.github.com>
2021-10-10 14:08:09 -0500

    created .gitignore file as well as two scripts for scripting on the course

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+// let country = "Mexico";
+// let continent = "America";
+// let population = 129829800;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let isIsland = false;
+// let language;
+
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// let firstName = "Bob";
+// console.log(firstName);
+
+// const firstName = "Omar";
+// const job = "software developer";
+// const birthYear = 1991;
+// const year = 2021;
+
+// const omarNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+// console.log(omarNew);
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🏄🏻‍♀️");
+} else {
+  console.log("You need Jesus, Sarah!");
+}
